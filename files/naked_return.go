3e2650c642abac927deb9d61d5cb18bb3a8d1ab1
benxdev <jike.exe@gmail.com>
2025-04-01 00:34:58 +0100

    1% better

---
+++ files/naked_return.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mathFunc(a, b int ) (prod int, sum int){
+	prod = a * b
+	sum =  a + b
+	return 
+}
+func main() {	 
+	var num1, num2 int
+	fmt.Print("Enter Num 1: ")
+	fmt.Scanf("%d\n", &num1)
+	fmt.Print("Enter Num 2: ")
+	fmt.Scanf("%d\n", &num2)
+	var prod, sum int = mathFunc(num1, num2) 
+	fmt.Printf("prod: %d\n sum: %d", prod, sum)
+}
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var a int
-	fmt.Print("Enter the value of a\n")
-	fmt.Scanf("%d", &a)
-	var p *int 	// pointer p (variable that stores the memory address of another variable)
-	p = &a 		// pointer p stores the "memory address" of variable a
-	var c = *p 	// c is declared and takes on the value of variable (a) that the pointer p is pointing to in memory
-	fmt.Printf("c copied as a: %d", c)
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
 }
