1c1e5069dd721d857e010f81f0b3c3bbeec6d2a8
benxdev <jike.exe@gmail.com>
2025-04-07 05:42:04 +0100

    nothing much today

---
+++ incomplete/Sqrt.go
@@ -0,0 +1,26 @@
+package main 
+import (
+	"fmt"
+)
+// Implementing the math.Sqrt() function from the Standard Library
+func Sqrt(x float64) (root float64) {
+	
+	if z := 1.0; z * z == x {
+		root = 1.0
+		return root
+	} else {
+		for z = 1.0; z < x; z++ {
+			root = ((z * z) - x) / 2 * z
+			
+		} 
+	}
+	return root
+}
+
+func main() {
+	var x float64
+	fmt.Print("Enter number to find square root:")
+	fmt.Scanf("%f", &x)
+	root := Sqrt(x)
+	fmt.Printf("The square root of %v is %v", x, root)
+}
