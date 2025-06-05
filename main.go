d7a82ad5f0eb788c88633462f94d9d799008d06d
Benedict Uduogu <jike.exe@gmail.com>
2025-06-05 20:58:54 +0100

    +

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type car struct {
-	Make string
-	Model string
-	Height int
-	Width int
-	Frontwheel Wheel
-	Backwheel Wheel
-	Category KindOfVehicle
-}
-type KindOfVehicle struct {
-	Kind string
-	Power float64	
-}
-type Wheel struct {
-	Radius int
-	Material string
-}
+var (
+	a = 930
+	b = 250.0
+)
 
-func main () {
-	myCar := car{}
-	var theType := myCar.Category.Kind
+func main() {
+	// a := 4
+	fmt.Printf("Type of a is %T, value of a is %[1]v\n", a)
+	fmt.Printf("Type of b is %T, value of b is %[1]v\n", b)
+
+	c:= a + int(b)
+	fmt.Println(c)
 }
