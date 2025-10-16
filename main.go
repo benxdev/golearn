2e201f91a0f9d4b23ab8c361e4e765949cd5c061
Ejike Uduogu <jike.exe@gmail.com>
2025-10-16 05:58:45 +0100

    back on this

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// var x int = 344
+
+// func main() {
+// 	var x = make([]int, 5, 50)
+// 	x = append(x, 67)
+// 	x[1] = 69
+// 	fmt.Println(x, len(x), cap(x))
+
+// }
+
+
 package main
-
-import (
-	"fmt"
-)
-
-var x int = 344
+import "fmt"
 
 func main() {
-	var x = make([]int, 5, 50)
-	x = append(x, 67)
-	x[10] = 69
-	fmt.Println(x, len(x), cap(x))
-
+	x := []string{"a", "b", "c", "d"}
+	y := x[:2]	//[a, b]
+	z := x[1:]	//[b, c, d]
+	x[1] = "y"	//x = [a, y, c, d]
+	y[0] = "x"	//y = []
+	z[1] = "z"
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
 }
