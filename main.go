d7ac02b66501cff9bce76042d1d7837f411561a7
Ejike Uduogu <jike.exe@gmail.com>
2025-10-24 21:58:39 +0100

    continue

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := []int{1, 2, 3, 4, 5}
-	y := make([]int, 2)
-	copy(y, x[3:])
-	fmt.Println("y = ", y)
+	var x int = 44
+	var y = string(x)
+	fmt.Println(y)
 }
