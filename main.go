ab66d4bbe5f93497d3b6d8c4e84edf883a4a911c
Benedict Uduogu <jike.exe@gmail.com>
2025-11-22 09:05:54 +0100

    on interfaces still

--- main.go
+++ main.go
@@ -18,8 +18,22 @@ var st = []string{
 	"Liquidity",
 }
 
+var st2 = []string{
+        "Akasha",    // 6
+        "Bane",      // 5
+        "Crystal",   // 7
+        "Dazzle",    // 7
+        "Enigma",    // 6
+        "Faceless",  // 8
+        "Gondar",    // 6
+        "Huskar",    // 6
+        "Invoker",   // 7
+        "Juggernaut",// 9
+        "Kunkka",    // 6
+    }
+
 type I interface {
-	doIt() []string
+	showName() string
 }
 
 type myList struct {
@@ -27,16 +41,30 @@ type myList struct {
 }
 
 
-func (m *myList.Names) doIt(c []string) {
-	m = &myList.Names
-	for i:=0; i<5; i++ {
-		fmt.Println(m)
+func (sh myList) showName() string {
+	for i:=0; i<len(sh.Names); i++ {
+		if len(sh.Names[i]) <= 6 {
+			fmt.Println(sh.Names[i])
+		} 
 	}
+	return "Too Large a string"
+}
+
+func (sh myList) removeSomeEntries() []string {
+	
 }
 
 func main() {
-	 list :=  myList {st}
-	var i I = 
-	c := &myList{st}
+	list := myList {
+		Names: st,
+	}
+	list2 := myList {
+		Names: st2,
+	}
+
+
+	var i, j I = list, list2
+	i.showName()
+	j.showName()
 
 }
