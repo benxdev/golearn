d89058e2f52c856f19e866de5921410e20a08f21
Ejike Uduogu <jike.exe@gmail.com>
2025-12-11 00:30:20 +0100

    ...

---
+++ files/interface02.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+type Ser interface {
+	fullNamer()
+}
+
+type Person struct {
+	FirstName string
+	LastName string
+}
+
+func (p Person) fullNamer() {
+	fmt.Print("first name = ")
+	fmt.Scanln(&p.FirstName)
+	fmt.Print("lastname = ")
+	fmt.Scanln(&p.LastName)
+	fmt.Print("fullname = " + p.FirstName + " " + p.LastName) 
+}
+
+func main() {
+	var p Person
+	var ser Ser = p // OR ser := Ser(p)
+	ser.fullNamer()
+}
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
 )
 
+func pString(c string) {
+	fmt.Printf("this input is a %T type", c)
+}
+func pInt(c int) {
+	fmt.Printf("this input is a %T type", c)
+}
 
 func main() {
-	
+	var i interface{}
+	fmt.Scan(&i)
+
+	switch v := i.(type) {
+	case string:
+		pString(v)
+	case int:
+		pInt(v)
+	default:
+		fmt.Print("invalid input type!")
+	// 	s, ok := i.(int16)
+	// 	fmt.Println(s, ok)
+	}
 }
